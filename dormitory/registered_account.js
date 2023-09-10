d635773a4d4f10c6a04fcc5f1ec0c28a579db9a8
j3sic4Cu7e <109582331+j3sic4Cu7e@users.noreply.github.com>
2023-09-10 10:34:58 -0700

    init

--- dormitory/registered_account.js
+++ dormitory/registered_account.js
@@ -40,4 +40,4 @@ const registered_accountsSchema = new Schema({
     }        
 });
 
-module.exports = mongoose.model('registered_accounts', registered_accountsSchema);
+module.exports = mongoose.dormitory('registered_accounts', registered_accountsSchema);
