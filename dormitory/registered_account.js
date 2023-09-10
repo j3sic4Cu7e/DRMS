88732d9d7b89f8598b16c61d3277cbc469b86c78
j3sic4Cu7e <109582331+j3sic4Cu7e@users.noreply.github.com>
2023-09-09 20:37:10 -0700

    init

---
+++ dormitory/registered_account.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const registered_accountSchema = new Schema({
+    tenant_name: {
+        type: String,
+        required: true,
+    },
+    tenant_username: {
+        type: String,
+        required: true,
+    },
+    tenant_password: {
+        type: String,
+        required: true,
+    }, 
+    tenant_contact: {
+        type: String,
+        required: true,
+    },
+    tenant_email: {
+        type: String,
+        required: true,
+    },
+    tenant_age: {
+        type: String,
+        required: true,
+    }, 
+    tenant_gender: {
+        type: String,
+        required: true,
+    },        
+})
+
+module.exports = mongoose.model('registered_account', registered_accountSchema);
---
+++ index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const registered_account = require('./dormitory/registered_account');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set('strictQuery', false);
+const connectDB = async ()=> {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected: ${conn.connection.host');
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send({DRMS: 'registered_account'});
+});
+
+app.get('/add-note', async (req,res) => {
+    try {
+        await registered_account.insertMany([
+            {
+                tenants_name: "Jessica Aoanan",
+                tenant_username: "Jessica",
+                tenant_password: "Aoanan",
+                tenant_contact: "09692109531",
+                tenant_email: "aoananjessica15@gmail.com",
+                tenant_age: "21",
+                tenant_gender: "Female",
+            }
+        ]);
+    } catch (error) {
+        console.log("err", + error);
+    }
+})
+
+
+app.get('/registered_account', async (req,res)=> {
+    const registered_account = await registered_account.find();
+    if (registered_account) {
+        res.json(registered_account)
+    } else {
+        res.send("Something went wrong");
+    }
+});
+connectDB().then(() => {
+    app.listen(PORT , () => {
+        console.log('listening on port ${PORT}')
+    })
+})
+
